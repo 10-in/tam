01ab0819c7022dfa470967402bd851dc6702582d
上海实印科技 <developer@10in.com>
2021-09-26 15:23:08 +0800

    优化语义识别

--- src/Factory.php
+++ src/Factory.php
@@ -7,11 +7,13 @@ use Exception;
 /**
  * @method static g(string $spell): int 根据拼音获得天干的索引
  * @method static z(string $spell): int 根据拼音获得地支的索引
+ * @method static type(string $spell): int 根据拼音获得五神(印比伤才杀)的索引
  */
 class Factory
 {
     const GM = ['jia' => 0, 'yi' => 1, 'bing' => 2, 'ding' => 3, 'wu' => 4, 'ji' => 5, 'geng' => 6, 'xin' => 7, 'ren' => 8, 'gui' => 9];
     const ZM = ['zi'=> 0, 'chou' => 1, 'yin'=> 2, 'mao' => 3, 'chen' => 4, 'si' => 5, 'wu' => 6, 'wei' => 7, 'shen' => 8, 'you'=> 9, 'xu' => 10, 'hai' => 11];
+    const Type = ['yin' => 0, 'bi' => 1, 'shang' => 2, 'cai' => 3, 'sha' => 4];
 
     /**
      * @throws Exception
@@ -22,6 +24,8 @@ class Factory
             return self::GM[$arguments];
         } elseif ($name == 'z') {
             return self::ZM[$arguments];
+        } elseif ($name == 'type') {
+            return self::Type[$arguments];
         }
         throw new Exception('method is not exist in ' . static::class);
     }
